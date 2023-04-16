5d12993a2d7247ec68b622282a2910d9e28134c8
zBilalz <s122572@ap.be>
2023-04-16 23:21:23 +0200

    Favoriet pagina aangemaakt

--- favoriet.js
+++ favoriet.js
@@ -1,30 +1,38 @@
+const apiUrl = 'https://fortnite-api.com/v2/cosmetics/br';
 
-let favoriet = document.getElementById("ster");
-let avatarNaam = document.getElementById("skin-name")
-let knopFavoriet = document.getElementsByClassName("buttonFavoriet")
-let starButton = document.createElement("button");
-    starButton.setAttribute("class","buttonFavoriet")
-    let star = document.createElement("i");
+async function getFortniteSkins() {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  const skins = data.data.filter(item => item.type.value === 'outfit');
+
+  const container = document.querySelector('.container');
+
+  for (let index = 0; index < 12; index++) {
+    const skin = skins[index];
+
+    if(skin.name != "null"){
+    const skinDiv = document.createElement('div');
+    skinDiv.classList.add('skin');
+
+    const skinImg = document.createElement('img')
+    skinImg.src = skin.images.icon;
+
+    const skinName = document.createElement('div');
+
+    skinName.classList.add('skin-name');
+    skinName.innerText = skin.name;
+
+    skinImg.addEventListener("click", () => {
+      window.open(`favoriet-overzicht.html?name=${encodeURIComponent(skin.name)}&image=${encodeURIComponent(skin.images.icon)}&backstory=${encodeURIComponent(skin.description)}`, "_self")})
+      
+     
   
-    star.setAttribute("class","bi bi-star");
-    star.setAttribute("id","0")
-    starButton.appendChild(star);
-    favoriet.appendChild(starButton);
-    
-   
-    
-  for (let i = 0; i < knopFavoriet.length; i++) {
-    knopFavoriet[i].addEventListener("click", () => {
-       
-        if (knopFavoriet[i].children[0].id == "0") {
-          knopFavoriet[i].children[0].setAttribute("class","bi bi-star-fill");
-          knopFavoriet[i].children[0].setAttribute("style","color: rgb(255, 210, 48)");
-          knopFavoriet[i].children[0].setAttribute("id","1");
-        }
-        else {
-          knopFavoriet[i].children[0].setAttribute("class","bi bi-star");
-          knopFavoriet[i].children[0].setAttribute("style","color: rgb(128, 128, 128)");
-          knopFavoriet[i].children[0].setAttribute("id","0");     
-        }
-      });
+    skinDiv.appendChild(skinImg);
+    skinDiv.appendChild(skinName);
+    container.appendChild(skinDiv);
     }
+  }
+}
+
+getFortniteSkins();
