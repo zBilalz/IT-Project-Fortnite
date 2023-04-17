52b0b3193ddb03723322e602cfe326e6fa07c103
zBilalz <s122572@ap.be>
2023-04-18 01:04:28 +0200

    Aanpassingen aan pagina's zoals layout en woorden naar engels vertalen

--- addToPFP.js
+++ addToPFP.js
@@ -4,7 +4,6 @@ let displayedCharacter = document.getElementById("skin-image").src;
 let button = document.getElementById("knopAvatar");
 let img = document.createElement("img");
 img.setAttribute("id","avatarImage");
-//<img id="avatarImage" src="" alt="">
 button.addEventListener("click",()=>{
     divAvatar.innerHTML = "";
 
