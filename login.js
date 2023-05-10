0b90fc3c7798617398e651b628ada7d9ae25db6a
Terryrichy55@gmail.com <terry.boateng@student.ap.be>
2023-05-10 09:31:53 +0200

    aanpassing

--- login.js
+++ login.js
@@ -20,3 +20,16 @@ function inloggen(event) {
 }
 
 document.getElementById("loginForm").addEventListener("submit", inloggen);
+
+
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click',()=> {
+    wrapper.classList.add('active')
+})
+
+loginlink.addEventListener('click',()=> {
+    wrapper.classList.remove('active')
+})
