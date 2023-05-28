e3647bd387442ce96b47e5674a2a523b18a6f36f
zBilalz <s122572@ap.be>
2023-05-28 15:57:42 +0200

    mongodb server & interfaces

---
+++ interfaces.ts
@@ -0,0 +1,31 @@
+import { ObjectId } from "mongodb";
+
+
+export interface User {
+    _id?:ObjectId,
+    username: string,
+    password: string
+}
+
+
+export interface Account {
+    _id?:ObjectId,
+    username:string,
+    profielfoto?:string,
+    favoriet?:Favoriet[],
+    blacklist?:Blacklist[]
+}
+
+export interface Blacklist {
+    naam:string,
+    reden:string
+}
+
+export interface Favoriet {
+    name:string,
+    notitie:string,
+    item1:string,
+    item2:string,
+    wins:number,
+    loses:number
+}
