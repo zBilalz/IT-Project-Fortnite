b4ef179277e419c6284f9896e18e0079e917d4ca
zBilalz <s122572@ap.be>
2023-05-31 08:27:46 +0200

    aanpassingen aan de css van modal box van note toevoegen

--- public/js/favorietOverzicht.js
+++ public/js/favorietOverzicht.js
@@ -5,18 +5,30 @@ let modalNote = document.getElementById("modalNote");
 let spanNote = document.getElementsByClassName("close")[0];
 let noteButton = document.getElementById("knopNotitie");
 
+
 function controle(event) {
     let note = document.getElementById("notitie").value;
     if (note.length < 1 ) {
         event.preventDefault();
-        errorNote.textContent = "Add a note"
+        errorNote.textContent = "Add a note";
     }
     else {
         
     }
 }
 
+function controleDelete (event) {
+    let notitie = document.getElementById("emptyNote").value;
+    if (notitie == "empty") {
+        event.preventDefault();
+        errorNote.textContent = "Notes already empty";
+    }
+   
+}
+
 document.getElementById("addNoteForum").addEventListener("submit", controle);
+
+document.getElementById("deleteNoteForum").addEventListener("submit", controleDelete);
 
 noteButton.onclick = function() {
     modalNote.style.display = "block";
